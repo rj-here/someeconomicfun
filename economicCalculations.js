4aa9a39fe4994c7eb2365c88216858820b58097e
rj-here <r1sh1j@my.yorku.ca>
2025-06-25 17:23:26 -0400

    Updates
    Added the first major function. More work to be done. Some tweaks.

---
+++ economicCalculations.js
@@ -0,0 +1,17 @@
+function demandSupply() {
+    var output = document.getElementById("output");
+    var demand = parseInt(prompt("What is the quantity demanded?"));
+    var supply = parseInt(prompt("What is the quantity supplied?"));
+    if (demand == supply) {
+        document.getElementById("result").innerHTML = "The quantity demanded & supplied is the same, so it's an equilibrium!";
+        output.innerHTML = "The quantity demanded & supplied is the same, so it's an equilibrium!";
+    }
+    else if (demand > supply) {
+        document.getElementById("result").innerHTML = "The quantity demanded is more than the quantity supplied, so it's a shortage!";
+        output.innerHTML = "The quantity demanded is more than the quantity supplied, so it's a shortage!";
+    }
+    else if (demand < supply) {
+        document.getElementById("result").innerHTML = "The quantity demanded is less than the quantity supplied, so it's a surplus!";
+        output.innerHTML = "The quantity demanded is less than the quantity supplied, so it's a surplus!";
+    }
+}
